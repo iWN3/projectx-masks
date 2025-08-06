1c8a140d7977baddfdf335869ec9a436e173445e
KodakZack <119061087+KodakZack@users.noreply.github.com>
2025-08-06 10:47:00 -0400

    Updated Config usage to support gender-specific clothing for gas masks and night vision goggles

--- config.lua
+++ config.lua
@@ -2,18 +2,29 @@ Config = Config or {}
 
 Config = {
 	Framework = "qb-core", -- "qb-core", "qbox", "esx" or "custom"
-    Inventory = "ox", -- "ox" or "qs" or "qb" or "esx" or "lj" or "ps"
-    Notification = "ox", -- "ox" or "qb" or "esx"
+	Inventory = "ox",    -- "ox" or "qs" or "qb" or "esx" or "lj" or "ps"
+	Notification = "ox", -- "ox" or "qb" or "esx"
 
 	["GasMask"] = {
 		["Item"] = "gasmask",
-		["Clothing"] = 129,
-		["Variation"] = 1,
+		["Clothing"] = {
+			male = { drawable = 129, texture = 1 },
+			female = { drawable = 130, texture = 0 }
+		},
 	},
-		
+
 	["NightVisionGoggles"] = {
 		["Item"] = "nightvision",
-		["Clothing"] = 119,
+		["Clothing"] = {
+			male = {
+				down = { drawable = 118, texture = 0 },
+				up   = { drawable = 119, texture = 0 }
+			},
+			female = {
+				down = { drawable = 117, texture = 0 },
+				up   = { drawable = 118, texture = 0 }
+			}
+		}
 	},
 
 	["NotificationDuration"] = 5000,
